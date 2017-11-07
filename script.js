0f049c732512c1d7624f9b1835e1f670d169d047
fakebenjay <fakebenjay@gmail.com>
2017-11-06 19:03:04 -0500

    frontpage pics added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('div.col-md-9').html("<div class='landmark' id='welcome'><h1>Marathon Map</h1><p>Click any of the landmarks on the map to see photos before and during the 2017 New York City Marathon!</p></div>")
+  $('div.col-md-9').html(`<div class='landmark' id='welcome'><h1>Marathon Map</h1><p>Click any of the landmarks on the map to see photos before and during the 2017 New York City Marathon!</p><img class='photo' alt='front' src='front-pics/${Math.round(Math.random()*21)}.jpg'></img></div>`)
 
   $("body").on("click", "input.button", function(e) {
     e.preventDefault()
